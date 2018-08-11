d1de3c678cc3a302b21663134195a2c02bcbf225
MM <marie0901@gmail.com>
2018-08-11 19:59:47 +0200

    remove manualy data

--- src/app.js
+++ src/app.js
@@ -11,13 +11,6 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
-store.dispatch(addExpense ({description: 'Water bill', amount: 300, createdAt: 17}));
-store.dispatch(addExpense ({description: 'Rent', amount: 1300, createdAt: 1}));
-store.dispatch(addExpense ({description: 'Gas bill', amount: 500, createdAt: 16700}));
-
-
-
-console.log(getVisibleExpenses(store.getState().expenses, store.getState().filters));
 
 const jsx = (
   <Provider store={store}>
