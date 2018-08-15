00ffe1bf180691b11d2ae217911e0107567bd6b5
MM <marie0901@gmail.com>
2018-08-15 19:21:20 +0200

    add set_expense action

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,14 +1,13 @@
 import uuid from 'uuid';
-import database from '../firebase/firebase'
-
+import database from '../firebase/firebase';
 
 // ADD_EXPENSE
 export const addExpense = (expense) => ({
   type: 'ADD_EXPENSE',
   expense
-})
+});
 
-export const startAddExpense = (expenseData ={}) => {
+export const startAddExpense = (expenseData = {}) => {
   return (dispatch) => {
     const {
       description = '',
@@ -16,7 +15,8 @@ export const startAddExpense = (expenseData ={}) => {
       amount = 0,
       createdAt = 0
     } = expenseData;
-    const expense = {description, note, amount, createdAt};
+    const expense = { description, note, amount, createdAt };
+
     return database.ref('expenses').push(expense).then((ref) => {
       dispatch(addExpense({
         id: ref.key,
@@ -27,18 +27,37 @@ export const startAddExpense = (expenseData ={}) => {
 };
 
 // REMOVE_EXPENSE
-export const removeExpense = (
-  {
-    id = ''
-  } = {}
-  ) => ({
+export const removeExpense = ({ id } = {}) => ({
   type: 'REMOVE_EXPENSE',
   id
-})
+});
 
 // EDIT_EXPENSE
-export const editExpense =(id,updates) => ({
+export const editExpense = (id, updates) => ({
   type: 'EDIT_EXPENSE',
   id,
   updates
-})
+});
+
+// SET_EXPENSES
+export const setExpenses = (expenses) => ({
+  type: 'SET_EXPENSES',
+  expenses
+});
+
+export const startSetExpenses = () => {
+  return (dispatch) => {
+    return database.ref('expenses').once('value').then((snapshot) => {
+      const expenses = [];
+
+      snapshot.forEach((childSnapshot) => {
+        expenses.push({
+          id: childSnapshot.key,
+          ...childSnapshot.val()
+        });
+      });
+
+      dispatch(setExpenses(expenses));
+    });
+  };
+};
--- src/reducers/expenses.js
+++ src/reducers/expenses.js
@@ -1,3 +1,5 @@
+// Expenses Reducer
+
 const expensesReducerDefaultState = [];
 
 export default (state = expensesReducerDefaultState, action) => {
@@ -7,22 +9,21 @@ export default (state = expensesReducerDefaultState, action) => {
         ...state,
         action.expense
       ];
-      case 'REMOVE_EXPENSE':
-        return state.filter(({ id })=>(action.id !== id));
-      case 'EDIT_EXPENSE':
-        return state.map (
-          (expense) => {
-            if (expense.id === action.id) {
-              return {
-                ...expense,
-                ...action.updates
-              };
-            }
-            else {
-              return expense;
-            };
-          }
-        );
+    case 'REMOVE_EXPENSE':
+      return state.filter(({ id }) => id !== action.id);
+    case 'EDIT_EXPENSE':
+      return state.map((expense) => {
+        if (expense.id === action.id) {
+          return {
+            ...expense,
+            ...action.updates
+          };
+        } else {
+          return expense;
+        };
+      });
+    case 'SET_EXPENSES':
+      return action.expenses;
     default:
       return state;
   }
