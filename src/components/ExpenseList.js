7c76e94c9cebc5e26877edc381c057960cc9883e
MM <marie0901@gmail.com>
2018-08-18 12:31:08 +0200

    final version with styles

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import ExpenseListItem from './ExpenseListItem'
-import selectExpenses from '../selectors/expenses'
-
+import ExpenseListItem from './ExpenseListItem';
+import selectExpenses from '../selectors/expenses';
 
 export const ExpenseList = (props) => (
-  <div>
-    <h1>Expense List</h1>
-    {
-      props.expenses.map((expense) => (
-        <ExpenseListItem key={expense.id} {...expense} />
-      ))
-    }
-    {props.expenses.length === 0 && <p >No expenses yet</p>}
+  <div className="content-container">
+    <div className="list-header">
+      <div className="show-for-mobile">Expenses</div>
+      <div className="show-for-desktop">Expense</div>
+      <div className="show-for-desktop">Amount</div>
+    </div>
+    <div className="list-body">
+      {
+        props.expenses.length === 0 ? (
+          <div className="list-item list-item--message">
+            <span>No expenses</span>
+          </div>
+        ) : (
+            props.expenses.map((expense) => {
+              return <ExpenseListItem key={expense.id} {...expense} />;
+            })
+          )
+      }
+    </div>
   </div>
 );
 
 const mapStateToProps = (state) => {
   return {
     expenses: selectExpenses(state.expenses, state.filters)
-
   };
 };
 
